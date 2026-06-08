10cd98068e50142a06cd00035b59cd2f381abbb1
Daniel Virguez <daniel-virguez@outlook.com>
2026-06-08 11:35:50 -0400

    refactor: Hacer todas las vistas completamente responsivas con tablas, modales y botones optimizados

--- views/matriculas/index.php
+++ views/matriculas/index.php
@@ -6,7 +6,7 @@
 <?php endif; ?>
 
 <?php if (($role === 'maestro' || $role === 'profesor' || $role === 'admin') && !empty($canManage)): ?>
-    <form class="form-inline" method="get" action="index.php">
+    <form class="form-inline" method="get" action="index.php" role="form">
         <input type="hidden" name="controller" value="matriculas">
         <input type="hidden" name="action" value="index">
         <?php if (!empty($materias)): ?>
@@ -24,7 +24,7 @@
         <?php endif; ?>
         <button class="btn btn-default" type="submit">Filtrar</button>
         <a class="btn btn-success" href="index.php?controller=matriculas&action=form"><span class="glyphicon glyphicon-plus"></span> Nueva asignacion</a>
-        <a class="btn btn-info" href="index.php?controller=matriculas&action=exportar<?php echo !empty($materiaId) ? '&materia_id=' . (int) $materiaId : ''; ?>"><span class="glyphicon glyphicon-download"></span> Exportar lista</a>
+        <a class="btn btn-info" href="index.php?controller=matriculas&action=exportar<?php echo !empty($materiaId) ? '&materia_id=' . (int) $materiaId : ''; ?>"><span class="glyphicon glyphicon-download"></span> Exportar</a>
     </form>
     <br>
 <?php endif; ?>
@@ -35,7 +35,7 @@
         <div class="panel-body">
             <form method="post" action="index.php?controller=matriculas&action=inscribir">
                 <div class="row">
-                    <div class="col-sm-8 form-group">
+                    <div class="col-xs-12 col-sm-8 form-group">
                         <label>Materia disponible</label>
                         <select class="form-control" name="materia_id" required>
                             <option value="">Seleccione...</option>
@@ -46,7 +46,7 @@
                             <?php endforeach; ?>
                         </select>
                     </div>
-                    <div class="col-sm-4 form-group">
+                    <div class="col-xs-12 col-sm-4 form-group">
                         <label>Créditos máximos</label>
                         <input class="form-control" type="text" value="<?php echo (int) $maxCreditos; ?>" disabled>
                     </div>
@@ -57,53 +57,57 @@
     </div>
 <?php endif; ?>
 
-<table class="table table-bordered table-striped">
-    <thead>
-    <tr>
-        <th>ID</th>
-        <th>Alumno</th>
-        <th>Materia</th>
-        <th>Docente</th>
-        <th>Fecha</th>
-        <th>Obj.1</th>
-        <th>Obj.2</th>
-        <th>Obj.3</th>
-        <th>Obj.4</th>
-        <?php if (!empty($canManage)): ?><th>Acciones</th><?php endif; ?>
-    </tr>
-    </thead>
-    <tbody>
-    <?php if (empty($rows)): ?>
-        <tr><td colspan="<?php echo !empty($canManage) ? 10 : 9; ?>">No hay registros disponibles.</td></tr>
-    <?php endif; ?>
-    <?php foreach ($rows as $r): ?>
+<div class="table-responsive">
+    <table class="table table-bordered table-striped table-hover">
+        <thead>
         <tr>
-            <td><?php echo (int) $r['id']; ?></td>
-            <td><?php echo htmlspecialchars($r['alumno_nombre'] . ' ' . $r['alumno_apellido']); ?></td>
-            <td><?php echo htmlspecialchars($r['materia_nombre']); ?></td>
-            <td><?php echo htmlspecialchars($r['docente_nombre'] . ' ' . $r['docente_apellido']); ?></td>
-            <td><?php echo htmlspecialchars($r['fecha_matricula']); ?></td>
-            <td><?php echo htmlspecialchars($r['obj1']); ?></td>
-            <td><?php echo htmlspecialchars($r['obj2']); ?></td>
-            <td><?php echo htmlspecialchars($r['obj3']); ?></td>
-            <td><?php echo htmlspecialchars($r['obj4']); ?></td>
-            <?php if (!empty($canManage)): ?>
-                <td>
-                    <button class="btn btn-info btn-xs" data-toggle="modal" data-target="#viewModal" onclick="viewRecord(<?php echo (int) $r['id']; ?>, '<?php echo htmlspecialchars($r['alumno_nombre'] . ' ' . $r['alumno_apellido']); ?>', '<?php echo htmlspecialchars($r['materia_nombre']); ?>', '<?php echo htmlspecialchars($r['docente_nombre'] . ' ' . $r['docente_apellido']); ?>', '<?php echo htmlspecialchars($r['fecha_matricula']); ?>', '<?php echo htmlspecialchars($r['obj1']); ?>', '<?php echo htmlspecialchars($r['obj2']); ?>', '<?php echo htmlspecialchars($r['obj3']); ?>', '<?php echo htmlspecialchars($r['obj4']); ?>')">
-                        <span class="glyphicon glyphicon-eye-open"></span> Ver
-                    </button>
-                    <a class="btn btn-primary btn-xs" href="index.php?controller=matriculas&action=form&id=<?php echo (int) $r['id']; ?>">
-                        <span class="glyphicon glyphicon-pencil"></span> Editar
-                    </a>
-                    <a class="btn btn-danger btn-xs" href="index.php?controller=matriculas&action=delete&id=<?php echo (int) $r['id']; ?>" onclick="return confirm('¿Eliminar registro?');">
-                        <span class="glyphicon glyphicon-trash"></span> Eliminar
-                    </a>
-                </td>
-            <?php endif; ?>
+            <th>ID</th>
+            <th>Alumno</th>
+            <th>Materia</th>
+            <th>Docente</th>
+            <th>Fecha</th>
+            <th>Obj.1</th>
+            <th>Obj.2</th>
+            <th>Obj.3</th>
+            <th>Obj.4</th>
+            <?php if (!empty($canManage)): ?><th>Acciones</th><?php endif; ?>
         </tr>
-    <?php endforeach; ?>
-    </tbody>
-</table>
+        </thead>
+        <tbody>
+        <?php if (empty($rows)): ?>
+            <tr><td colspan="<?php echo !empty($canManage) ? 10 : 9; ?>">No hay registros disponibles.</td></tr>
+        <?php endif; ?>
+        <?php foreach ($rows as $r): ?>
+            <tr>
+                <td><?php echo (int) $r['id']; ?></td>
+                <td><?php echo htmlspecialchars($r['alumno_nombre'] . ' ' . $r['alumno_apellido']); ?></td>
+                <td><?php echo htmlspecialchars($r['materia_nombre']); ?></td>
+                <td><?php echo htmlspecialchars($r['docente_nombre'] . ' ' . $r['docente_apellido']); ?></td>
+                <td><?php echo htmlspecialchars($r['fecha_matricula']); ?></td>
+                <td><?php echo htmlspecialchars($r['obj1']); ?></td>
+                <td><?php echo htmlspecialchars($r['obj2']); ?></td>
+                <td><?php echo htmlspecialchars($r['obj3']); ?></td>
+                <td><?php echo htmlspecialchars($r['obj4']); ?></td>
+                <?php if (!empty($canManage)): ?>
+                    <td>
+                        <div class="btn-group btn-group-xs" role="group">
+                            <button class="btn btn-info" data-toggle="modal" data-target="#viewModal" onclick="viewRecord(<?php echo (int) $r['id']; ?>, '<?php echo htmlspecialchars($r['alumno_nombre'] . ' ' . $r['alumno_apellido']); ?>', '<?php echo htmlspecialchars($r['materia_nombre']); ?>', '<?php echo htmlspecialchars($r['docente_nombre'] . ' ' . $r['docente_apellido']); ?>', '<?php echo htmlspecialchars($r['fecha_matricula']); ?>', '<?php echo htmlspecialchars($r['obj1']); ?>', '<?php echo htmlspecialchars($r['obj2']); ?>', '<?php echo htmlspecialchars($r['obj3']); ?>', '<?php echo htmlspecialchars($r['obj4']); ?>')" title="Ver detalles">
+                                <span class="glyphicon glyphicon-eye-open"></span>
+                            </button>
+                            <a class="btn btn-primary" href="index.php?controller=matriculas&action=form&id=<?php echo (int) $r['id']; ?>" title="Editar">
+                                <span class="glyphicon glyphicon-pencil"></span>
+                            </a>
+                            <a class="btn btn-danger" href="index.php?controller=matriculas&action=delete&id=<?php echo (int) $r['id']; ?>" onclick="return confirm('¿Eliminar registro?');" title="Eliminar">
+                                <span class="glyphicon glyphicon-trash"></span>
+                            </a>
+                        </div>
+                    </td>
+                <?php endif; ?>
+            </tr>
+        <?php endforeach; ?>
+        </tbody>
+    </table>
+</div>
 
 <?php if (!empty($pages) && $pages > 1): ?>
     <nav>
@@ -127,47 +131,45 @@
                 <h4 class="modal-title"><span class="glyphicon glyphicon-info-sign"></span> Detalles de la Matrícula</h4>
             </div>
             <div class="modal-body">
-                <div class="row">
-                    <div class="col-sm-6">
-                        <div class="form-group">
-                            <label><strong>ID:</strong></label>
-                            <p id="viewId"></p>
-                        </div>
-                        <div class="form-group">
-                            <label><strong>Alumno:</strong></label>
-                            <p id="viewAlumno"></p>
-                        </div>
-                        <div class="form-group">
-                            <label><strong>Materia:</strong></label>
-                            <p id="viewMateria"></p>
-                        </div>
-                        <div class="form-group">
-                            <label><strong>Docente:</strong></label>
-                            <p id="viewDocente"></p>
-                        </div>
-                        <div class="form-group">
-                            <label><strong>Fecha Matrícula:</strong></label>
-                            <p id="viewFecha"></p>
-                        </div>
-                    </div>
-                    <div class="col-sm-6">
-                        <div class="form-group">
-                            <label><strong>Objetivo 1:</strong></label>
-                            <p id="viewObj1"></p>
-                        </div>
-                        <div class="form-group">
-                            <label><strong>Objetivo 2:</strong></label>
-                            <p id="viewObj2"></p>
-                        </div>
-                        <div class="form-group">
-                            <label><strong>Objetivo 3:</strong></label>
-                            <p id="viewObj3"></p>
-                        </div>
-                        <div class="form-group">
-                            <label><strong>Objetivo 4:</strong></label>
-                            <p id="viewObj4"></p>
-                        </div>
-                    </div>
+                <div class="table-responsive">
+                    <table class="table table-condensed">
+                        <tr>
+                            <td><strong>ID:</strong></td>
+                            <td id="viewId"></td>
+                        </tr>
+                        <tr>
+                            <td><strong>Alumno:</strong></td>
+                            <td id="viewAlumno"></td>
+                        </tr>
+                        <tr>
+                            <td><strong>Materia:</strong></td>
+                            <td id="viewMateria"></td>
+                        </tr>
+                        <tr>
+                            <td><strong>Docente:</strong></td>
+                            <td id="viewDocente"></td>
+                        </tr>
+                        <tr>
+                            <td><strong>Fecha Matrícula:</strong></td>
+                            <td id="viewFecha"></td>
+                        </tr>
+                        <tr>
+                            <td><strong>Objetivo 1:</strong></td>
+                            <td id="viewObj1"></td>
+                        </tr>
+                        <tr>
+                            <td><strong>Objetivo 2:</strong></td>
+                            <td id="viewObj2"></td>
+                        </tr>
+                        <tr>
+                            <td><strong>Objetivo 3:</strong></td>
+                            <td id="viewObj3"></td>
+                        </tr>
+                        <tr>
+                            <td><strong>Objetivo 4:</strong></td>
+                            <td id="viewObj4"></td>
+                        </tr>
+                    </table>
                 </div>
             </div>
             <div class="modal-footer">
